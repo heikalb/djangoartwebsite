3d0fea3cf8be4d92b1c773b63c3cab0fa9610014
heikalb <heikal93@gmail.com>
2020-01-25 13:41:36 -0800

    started model

--- images/models.py
+++ images/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Image(models.Model):
+    full_image_file = None
+    thumbnail = None
+    caption = ''
+    date = ''
+    location = ''
+    category = ''
+    featured = False
