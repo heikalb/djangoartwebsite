9f729e542ff39d5906d6bdea482d5fb1d4d58df4
heikalb <heikal93@gmail.com>
2020-01-25 22:42:09 -0800

    + views

--- images/views.py
+++ images/views.py
@@ -4,6 +4,32 @@ from .models import Image
 
 
 def home_view(request):
-    images = Image.objects.all()
+    images = Image.objects.filter(featured=True)
     context = {'images': images}
     return render(request, 'images/image_list.html', context)
+
+
+def list_view(request, category):
+    images = Image.objects.filter(category=category).order_by('ranking')
+    context = {'images': images}
+    return render(request, 'images/image_list.html', context)
+
+
+def birds_view(request):
+    return list_view(request, 'Birds')
+
+
+def waterfowls_view(request):
+    return list_view(request, 'Waterfowls')
+
+
+def madison_view(request):
+    return list_view(request, 'Madison, WI')
+
+
+def madison_view(request):
+    return list_view(request, 'Other')
+
+
+def madison_view(request):
+    return list_view(request, 'Paintings')
