3bb9cb8ff95a3bbb9548ddff41d9fcf992413a1c
heikalb <heikal93@gmail.com>
2020-01-25 23:07:17 -0800

    +description

--- images/views.py
+++ images/views.py
@@ -5,31 +5,44 @@ from .models import Image
 
 def home_view(request):
     images = Image.objects.filter(featured=True)
-    context = {'images': images}
+    description = "Photography from Madison, Wisconsin, USA, to Greater " \
+                  "Vancouver, British Columbia, Canada. Fixated upon ducks, " \
+                  "birds, pretty bugs, trees, leaves, water, reflections, winter " \
+                  "and dusks."
+
+    context = {'images': images, 'description': description}
     return render(request, 'images/image_list.html', context)
 
 
-def list_view(request, category):
+def list_view(request, category, description):
     images = Image.objects.filter(category=category).order_by('ranking')
-    context = {'images': images}
+
+    context = {'images': images, 'description': description}
+
     return render(request, 'images/image_list.html', context)
 
 
 def birds_view(request):
-    return list_view(request, 'Birds')
+    description = 'Birds, from House Sparrows to Sandhill Cranes.'
+    return list_view(request, 'Birds', description)
 
 
 def waterfowls_view(request):
-    return list_view(request, 'Waterfowls')
+    description = 'Birds, but on water.'
+
+    return list_view(request, 'Waterfowls', description)
 
 
 def madison_view(request):
-    return list_view(request, 'Madison, WI')
+    description = 'Sights from the city between two lakes.'
+    return list_view(request, 'Madison, WI', description)
 
 
 def other_view(request):
-    return list_view(request, 'Other')
+    description = 'Flora, insects, other places ...'
+    return list_view(request, 'Other', description)
 
 
 def paintings_view(request):
-    return list_view(request, 'Paintings')
+    description = 'Acrylic canvas paintings.'
+    return list_view(request, 'Paintings', description)
