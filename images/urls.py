9a96009ce356f722f56706e407169f5a7de23ae8
heikalb <heikal93@gmail.com>
2020-01-25 22:47:45 -0800

    +urls

--- images/urls.py
+++ images/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('photography', home_view, name='photography'),
+    path('birds', birds_view, name='birds'),
+    path('waterfowls', waterfowls_view, name='waterfowls'),
+    path('madison', madison_view, name='madison'),
+    path('other', other_view, name='other'),
+    path('paintings', paintings_view, name='paintings')
 ]
