5801ce9ae92c93b4fc18611168d95f801b613a6d
heikalb <heikal93@gmail.com>
2020-01-25 18:18:01 -0800

    added static files

--- djangoartwebsite/urls.py
+++ djangoartwebsite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from images.views import home_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', home_view, name='home')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
