58ebd705952f93191a6f7a8e8890bb8c12caf2c1
heikalb <heikal93@gmail.com>
2020-01-28 10:18:47 -0800

    +view

--- visitor_messages/views.py
+++ visitor_messages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def contact_view(request):
+    form = None
+
+    context = {'form': form}
+
+    return render(request, 'visitor_messages/contact.html',context)
