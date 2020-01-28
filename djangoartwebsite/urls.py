9c36e0cc11eb1a11b3619abb82ed0096a06c8e2e
heikalb <heikal93@gmail.com>
2020-01-28 11:31:34 -0800

    added contact url

--- djangoartwebsite/urls.py
+++ djangoartwebsite/urls.py
@@ -22,5 +22,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('images.urls')),
+    path('', include('visitor_messages.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
