a09c3f55663cf2fae4d302b5b7acc134d3eeafe5
heikalb <heikal93@gmail.com>
2020-01-29 10:30:39 -0800

    +success message

--- visitor_messages/views.py
+++ visitor_messages/views.py
@@ -5,11 +5,13 @@ from .forms import ContactForm
 
 def contact_view(request):
     form = ContactForm(request.POST or None)
+    success_msg = ''
 
     if form.is_valid():
         form.save()
         form = ContactForm()
+        success_msg = 'Thank you for your message'
 
-    context = {'form': form}
+    context = {'form': form, 'success_msg': success_msg}
 
     return render(request, 'visitor_messages/contact.html', context)
