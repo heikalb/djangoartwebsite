1f3d24c96812efd02964be631fc83be12b554aeb
heikalb <heikal93@gmail.com>
2020-01-29 11:11:50 -0800

    +date created

--- visitor_messages/models.py
+++ visitor_messages/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.timezone import now
 # Create your models here.
 
 
@@ -7,3 +7,4 @@ class VisitorMessage(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     message = models.TextField(default='')
+    date_created = models.DateField(default=now, null=True, blank=True)
